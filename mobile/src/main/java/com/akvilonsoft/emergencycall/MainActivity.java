0325fea9915bef2d1035bf3fd416064b0060ed6b
skvaznikov <evgeny.skvaznikov.ext@devoteam.de>
2015-03-26 16:37:47 +0100

    Map implemented

--- mobile/src/main/java/com/akvilonsoft/emergencycall/MainActivity.java
+++ mobile/src/main/java/com/akvilonsoft/emergencycall/MainActivity.java
@@ -1,23 +1,31 @@
 package com.akvilonsoft.emergencycall;
 
-import android.app.AlertDialog;
 import android.app.FragmentTransaction;
-import android.content.DialogInterface;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationManager;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
+import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.*;
+import com.google.android.gms.maps.model.*;
 
 
-public class MainActivity extends ActionBarActivity {
-
+public class MainActivity extends ActionBarActivity  {
+    private static GoogleMap mMap;
+    public static FragmentManager fragmentManager;
+    private static Double latitude, longitude;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fragmentManager = getSupportFragmentManager();
     }
 
 
@@ -42,14 +50,34 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
-    public void showAlert(View v) {
-
+    public void goHome(View v) {
+        latitude = 26.78;
+        longitude = 72.56;
         MapFragment mMapFragment = MapFragment.newInstance();
         FragmentTransaction fragmentTransaction =
                 getFragmentManager().beginTransaction();
         fragmentTransaction.add(R.id.scrollView, mMapFragment);
-        fragmentTransaction.commit();
 
+        LatLng sydney = new LatLng(48.520665, 8.833652);
+
+        if (mMap == null) {
+            // Try to obtain the map from the SupportMapFragment.
+            mMap = ((SupportMapFragment) MainActivity.fragmentManager
+                    .findFragmentById(R.id.location_map)).getMap();
+            // Check if we were successful in obtaining the map.
+            if (mMap != null)
+                setUpMap();
+        }
+
+        mMap.setMyLocationEnabled(true);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 20));
+
+        mMap.addMarker(new MarkerOptions()
+                .title("My home")
+                .snippet("The best home in Bondorf.")
+                .position(sydney));
+        mMap.setTrafficEnabled(true);
+        fragmentTransaction.commit();
 /*        new AlertDialog.Builder(this)
                 .setTitle("Delete entry")
                 .setMessage("Are you sure you want to delete this entry?")
@@ -66,4 +94,103 @@ public class MainActivity extends ActionBarActivity {
                 .setIcon(android.R.drawable.ic_dialog_alert)
                 .show();*/
     }
+    /**
+     * This is where we can add markers or lines, add listeners or move the
+     * camera.
+     * <p>
+     * This should only be called once and when we are sure that {@link #mMap}
+     * is not null.
+     */
+
+    /***** Sets up the map if it is possible to do so *****/
+    public static void setUpMapIfNeeded() {
+        // Do a null check to confirm that we have not already instantiated the map.
+        if (mMap == null) {
+            // Try to obtain the map from the SupportMapFragment.
+            mMap = ((SupportMapFragment) MainActivity.fragmentManager
+                    .findFragmentById(R.id.location_map)).getMap();
+            // Check if we were successful in obtaining the map.
+            if (mMap != null)
+                setUpMap();
+        }
+    }
+    private static void setUpMap() {
+        // For showing a move to my loction button
+        mMap.setMyLocationEnabled(true);
+        // For dropping a marker at a point on the Map
+        mMap.addMarker(new MarkerOptions().position(new LatLng(latitude, longitude)).title("My Home").snippet("Home Address"));
+        // For zooming automatically to the Dropped PIN Location
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(latitude,
+                longitude), 12.0f));
+    }
+
+    public Location getLocation() {
+        // flag for GPS status
+        boolean isGPSEnabled = true;
+
+        // flag for network status
+        boolean isNetworkEnabled = false;
+       LocationManager locationManager;
+        Location location = null; // location
+        try {
+            locationManager = (LocationManager) this
+                    .getSystemService(Context.LOCATION_SERVICE);
+
+            // getting GPS status
+            isGPSEnabled = locationManager
+                    .isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+         //   Log.v("isGPSEnabled", "=" + isGPSEnabled);
+
+            // getting network status
+            isNetworkEnabled = locationManager
+                    .isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+        //    Log.v("isNetworkEnabled", "=" + isNetworkEnabled);
+
+            if (isGPSEnabled == false && isNetworkEnabled == false) {
+                // no network provider is enabled
+            } else {
+ //               this.canGetLocation = true;
+ /*               if (isNetworkEnabled) {
+                    location=null;
+                    locationManager.requestLocationUpdates(
+                            LocationManager.NETWORK_PROVIDER,
+                            MIN_TIME_BW_UPDATES,
+                            MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+                    Log.d("Network", "Network");
+                    if (locationManager != null) {
+                        location = locationManager
+                                .getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                        if (location != null) {
+                            latitude = location.getLatitude();
+                            longitude = location.getLongitude();
+                        }
+                    }
+                }*/
+                // if GPS Enabled get lat/long using GPS Services
+                if (isGPSEnabled) {
+                    location=null;
+                    if (location == null) {
+                        //locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1L, 1.0f, this);
+                     //   Log.d("GPS Enabled", "GPS Enabled");
+                        if (locationManager != null) {
+                            location = locationManager
+                                    .getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                            if (location != null) {
+                                latitude = location.getLatitude();
+                                longitude = location.getLongitude();
+                            }
+                        }
+                    }
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return location;
+    }
+
 }
