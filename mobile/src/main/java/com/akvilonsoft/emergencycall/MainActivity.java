3b7b02cb07a3e027e36d3b93b57888641276c330
skvaznikov <evgeny.skvaznikov.ext@devoteam.de>
2015-04-02 13:33:58 +0200

    Map implemented

--- mobile/src/main/java/com/akvilonsoft/emergencycall/MainActivity.java
+++ mobile/src/main/java/com/akvilonsoft/emergencycall/MainActivity.java
@@ -2,16 +2,20 @@ package com.akvilonsoft.emergencycall;
 
 import android.app.FragmentTransaction;
 import android.content.Context;
+import android.content.Intent;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
+import android.net.Uri;
+import android.provider.Settings;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
-import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.*;
 import com.google.android.gms.maps.model.*;
@@ -21,13 +25,26 @@ public class MainActivity extends ActionBarActivity  {
     private static GoogleMap mMap;
     public static FragmentManager fragmentManager;
     private static Double latitude, longitude;
+    private Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         fragmentManager = getSupportFragmentManager();
+        context = this.getBaseContext();
+        turnGPSOn();
     }
+    private void turnGPSOn(){
+        String provider = Settings.Secure.getString(getContentResolver(), Settings.Secure.LOCATION_PROVIDERS_ALLOWED);
 
+    //    if(!provider.contains("gps")){ //if gps is disabled
+            final Intent poke = new Intent();
+            poke.setClassName("com.android.settings", "com.android.settings.widget.SettingsAppWidgetProvider");
+            poke.addCategory(Intent.CATEGORY_ALTERNATIVE);
+            poke.setData(Uri.parse("3"));
+            sendBroadcast(poke);
+  //      }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -50,15 +67,15 @@ public class MainActivity extends ActionBarActivity  {
 
         return super.onOptionsItemSelected(item);
     }
-    public void goHome(View v) {
-        latitude = 26.78;
-        longitude = 72.56;
+    public void goHome() {
+   //     latitude = 26.78;
+   //     longitude = 72.56;
         MapFragment mMapFragment = MapFragment.newInstance();
         FragmentTransaction fragmentTransaction =
                 getFragmentManager().beginTransaction();
         fragmentTransaction.add(R.id.scrollView, mMapFragment);
-
-        LatLng sydney = new LatLng(48.520665, 8.833652);
+        LatLng sydney = new LatLng(latitude, longitude);
+       // LatLng sydney = new LatLng(48.520665, 8.833652);
 
         if (mMap == null) {
             // Try to obtain the map from the SupportMapFragment.
@@ -77,7 +94,7 @@ public class MainActivity extends ActionBarActivity  {
                 .snippet("The best home in Bondorf.")
                 .position(sydney));
         mMap.setTrafficEnabled(true);
-        fragmentTransaction.commit();
+ //       fragmentTransaction.commit();
 /*        new AlertDialog.Builder(this)
                 .setTitle("Delete entry")
                 .setMessage("Are you sure you want to delete this entry?")
@@ -124,73 +141,65 @@ public class MainActivity extends ActionBarActivity  {
                 longitude), 12.0f));
     }
 
-    public Location getLocation() {
+    public void getLocation(View v) {
         // flag for GPS status
-        boolean isGPSEnabled = true;
+    //    Intent intent = new Intent("android.location.GPS_ENABLED_CHANGE");
+      //  intent.putExtra("enabled", true);
+   //     sendBroadcast(intent);
+        Location  myLocation;
+        LocationManager locationManager;
+       LocationListener locListener;
+        locationManager = (LocationManager) this.getBaseContext().getSystemService(Context.LOCATION_SERVICE);
+        locListener = new LocationListener() {
+            @Override
+            public void onStatusChanged(String provider, int status,
+                                        Bundle extras) {
+            }
+            @Override
+            public void onProviderEnabled(String provider) {
+            }
+            @Override
+            public void onProviderDisabled(String provider) {
+            }
+            @Override
+            public void onLocationChanged(Location location) {
+                Toast.makeText(context, "Your Location is - \nLat: " + location.getLatitude() + "\nLong: " + location.getLongitude(), Toast.LENGTH_LONG).show();
+                longitude = location.getLongitude();
+                latitude = location.getLatitude();
+                goHome();
+            }
+        };
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000L, 1f, locListener);
+   //     locationManager.requestLocationUpdates(
+   //             LocationManager.GPS_PROVIDER, 1000, 1, locListener);
+  //      mobileLocation = locManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+  //      locationManager = (LocationManager) this.getBaseContext()
+  //              .getSystemService(LOCATION_SERVICE);
+  //      Location location = locationManager.getLastKnownLocation("gps");
 
+        boolean isGPSEnabled = true;
+   //     turnGPSOn();
+        GPSTracker gps;
         // flag for network status
         boolean isNetworkEnabled = false;
-       LocationManager locationManager;
-        Location location = null; // location
-        try {
-            locationManager = (LocationManager) this
-                    .getSystemService(Context.LOCATION_SERVICE);
+        //gps = new GPSTracker(MainActivity.this);
 
-            // getting GPS status
-            isGPSEnabled = locationManager
-                    .isProviderEnabled(LocationManager.GPS_PROVIDER);
+        // Check if GPS enabled
+ /*       if(gps.canGetLocation()) {
 
-         //   Log.v("isGPSEnabled", "=" + isGPSEnabled);
+            latitude = gps.getLatitude();
+            longitude = gps.getLongitude();
+            goHome();
+            // \n is for new line
+   //         Toast.makeText(getApplicationContext(), "Your Location is - \nLat: " + latitude + "\nLong: " + longitude, Toast.LENGTH_LONG).show();
+        } else {
+            // Can't get location.
+            // GPS or network is not enabled.
+            // Ask user to enable GPS/network in settings.
+            gps.showSettingsAlert();
 
-            // getting network status
-            isNetworkEnabled = locationManager
-                    .isProviderEnabled(LocationManager.NETWORK_PROVIDER);
-
-        //    Log.v("isNetworkEnabled", "=" + isNetworkEnabled);
-
-            if (isGPSEnabled == false && isNetworkEnabled == false) {
-                // no network provider is enabled
-            } else {
- //               this.canGetLocation = true;
- /*               if (isNetworkEnabled) {
-                    location=null;
-                    locationManager.requestLocationUpdates(
-                            LocationManager.NETWORK_PROVIDER,
-                            MIN_TIME_BW_UPDATES,
-                            MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
-                    Log.d("Network", "Network");
-                    if (locationManager != null) {
-                        location = locationManager
-                                .getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-                        if (location != null) {
-                            latitude = location.getLatitude();
-                            longitude = location.getLongitude();
-                        }
-                    }
-                }*/
-                // if GPS Enabled get lat/long using GPS Services
-                if (isGPSEnabled) {
-                    location=null;
-                    if (location == null) {
-                        //locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1L, 1.0f, this);
-                     //   Log.d("GPS Enabled", "GPS Enabled");
-                        if (locationManager != null) {
-                            location = locationManager
-                                    .getLastKnownLocation(LocationManager.GPS_PROVIDER);
-                            if (location != null) {
-                                latitude = location.getLatitude();
-                                longitude = location.getLongitude();
-                            }
-                        }
-                    }
-                }
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return location;
+        }*/
     }
+
 
 }
